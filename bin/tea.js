8b56f19c7e2214d314d47ef140dfaeda5d799cd4
Harvey Zack <laozhaochaguan@hotmail.com>
2018-01-14 13:48:36 +0800

    update

--- bin/tea.js
+++ bin/tea.js
@@ -1,2 +1,39 @@
 #!/usr/bin/env node
-console.log(0);
+const meow = require('meow');
+const run = require('../lib/run');
+
+const cli = meow(
+  `
+    Use official templates
+      $ tea <template-name> <project-name>
+
+    Use custom templates
+      $ tea <username/repo> <project-name>
+
+    Use local templates
+      $ tea <~/fs/path/to-custom-template> <project-name>
+    
+    Show current version
+      $ tea -v
+
+    Show help
+      $ tea -h
+`,
+  {
+    flags: {
+      version: {
+        type: 'boolean',
+        alias: 'v'
+      },
+      help: {
+        type: 'boolean',
+        alias: 'h'
+      }
+    }
+  }
+);
+
+run({
+  templateUrl: cli.input[0],
+  projectName: cli.input[1]
+});
